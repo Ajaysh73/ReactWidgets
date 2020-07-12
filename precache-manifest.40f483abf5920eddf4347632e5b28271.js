19df15b208702c46899e3c5c52adc9ff3364bff4
Siya Sharma <ajaysh73@gmail.com>
2020-07-12 14:37:37 -0400

    Updates

---
+++ precache-manifest.40f483abf5920eddf4347632e5b28271.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f67bff8ab425a873887c71f66794125",
+    "url": "/ReactWidgets/index.html"
+  },
+  {
+    "revision": "7cc4ae36b3239678a882",
+    "url": "/ReactWidgets/static/js/2.4088855b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactWidgets/static/js/2.4088855b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74faed74e9db44e729e2",
+    "url": "/ReactWidgets/static/js/main.678c12f0.chunk.js"
+  },
+  {
+    "revision": "7478773b979e8f87bbbc",
+    "url": "/ReactWidgets/static/js/runtime-main.7b90450a.js"
+  }
+]);
